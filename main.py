a81a5348c7f9be0903cde9511d8f4af36cbbac78
Seamus Keating <seamusakeating@gmail.com>
2019-04-27 00:56:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Main Python Code
 # This file inports the CSV file and 
-
 import csv
-import pandas as pd
+import pandas as pd # allows input/output of a CSV file and processing
+import numpy as np # allows for linear algebra
+import matplotlib.pyplot as plt #allows for visualisation
 data = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
 print (data)
