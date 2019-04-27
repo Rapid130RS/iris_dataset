f8c3b91140241b43b507b3c55b8c8f163276968c
Seamus Keating <seamusakeating@gmail.com>
2019-04-27 02:12:15 +0100

    test seaborn plots

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import pandas as pd # allows input/output of a CSV file and processing
 import numpy as np # allows for linear algebra
 import matplotlib.pyplot as plt #allows for visualisation
 iris = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
-#print (data) #print table
-sns.set()
-iris = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
-sns.jointplot(x="sepal_length", y="petal_length", data=iris);
+#sns.jointplot(x="sepal_length", y="petal_length", data=iris)
+sns.boxplot(x="sepal_length", y="petal_length", data=iris)
+sns.scatterplot(x="sepal_length", y="petal_length", data=iris)
 
