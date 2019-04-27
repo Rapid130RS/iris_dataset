1b9814b0e96a545448b920ddd4aae9eae8700f2c
Seamus Keating <seamusakeating@gmail.com>
2019-04-27 02:26:25 +0100

    Update to test pairplot

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Main Python Code
-# This file inports the CSV file and 
 import csv
 import pandas as pd # allows input/output of a CSV file and processing
 import numpy as np # allows for linear algebra
+import seaborn as sns
 import matplotlib.pyplot as plt #allows for visualisation
 iris = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
-#sns.jointplot(x="sepal_length", y="petal_length", data=iris)
-sns.boxplot(x="sepal_length", y="petal_length", data=iris)
-sns.scatterplot(x="sepal_length", y="petal_length", data=iris)
-
+#print (data) #print table
+sns.set()
+iris = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
+sns.jointplot(x="sepal_length", y="petal_length", data=iris);
+sns.boxplot(x="sepal_length", y="petal_length", data=iris);
+#sns.scatterplot(x="sepal_length", y="petal_length", data=iris);
+sns.pairplot(data=iris, hue="species");
