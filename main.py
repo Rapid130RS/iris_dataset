83d6dca91e6cb2e62992f09718d6fb68d2d81092
Seamus Keating <seamusakeating@gmail.com>
2019-04-27 01:51:59 +0100

    Update with Seaborn Lib

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ import csv
 import pandas as pd # allows input/output of a CSV file and processing
 import numpy as np # allows for linear algebra
 import matplotlib.pyplot as plt #allows for visualisation
-data = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
-print (data)
+iris = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
+#print (data) #print table
+sns.set()
+iris = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
+sns.jointplot(x="sepal_length", y="petal_length", data=iris);
+
